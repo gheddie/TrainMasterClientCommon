0e25adcb5a2d1f61e36626317a3eb22915de533c
Sts <Sts@YellowMeerkat.sternico.local>
2019-11-30 17:46:03 +0100

    123

--- src/main/java/de/gravitex/trainmaster/common/RailItemSequcenFormatter.java
+++ src/main/java/de/gravitex/trainmaster/common/RailItemSequcenFormatter.java
@@ -7,6 +7,9 @@ public class RailItemSequcenFormatter {
 	public static String format(String trackName, List<String> identifiers) {
 
 		String result = "["+trackName+"]::";
+		if ((identifiers == null) || (identifiers.size() == 0)) {
+			return result + "#BLANK#";	
+		}
 		for (String identifier : identifiers) {
 			result += "["+identifier+"]";
 		}
